73d010b2357077a90c7d5d4dec99d627b170eb35
atipchao <atip.chaogunhar@cookcountyil.gov>
2023-12-25 15:30:22 -0600

    Inplemented Delete People

--- Intro_Blazor_WA_SA_7/Server/Controllers/PeopleController.cs
+++ Intro_Blazor_WA_SA_7/Server/Controllers/PeopleController.cs
@@ -23,9 +23,9 @@ namespace Intro_Blazor_WA_SA_7.Server.Controllers
         }
 
         [HttpGet("{id}", Name="GetPerson")]
-        public async Task<ActionResult<List<Person>>> Get(int id)
+        public async Task<ActionResult<Person>> Get(int id)
         {
-            return await _dbContext.People.Where(s => s.Id == id).ToListAsync();
+            return await _dbContext.People.Where(s => s.Id == id).FirstOrDefaultAsync();
         }
 
         [HttpPost]
@@ -36,14 +36,16 @@ namespace Intro_Blazor_WA_SA_7.Server.Controllers
             return new CreatedAtRouteResult("GetPerson", new { id = person.Id }, person);
         }
 
-        [HttpPut] async Task<ActionResult> Put(Person person)
+        [HttpPut] 
+        public async Task<ActionResult> Put(Person person)
         {
             _dbContext.Entry(person).State = EntityState.Modified;
             await _dbContext.SaveChangesAsync();
             return NoContent();
         }
 
-        [HttpDelete("{id}")] async Task<ActionResult> Delete(int id)
+        [HttpDelete("{id}")] 
+        public async Task<ActionResult> Delete(int id)
         {
             var person = new Person { Id = id };
             _dbContext.Remove(person);
