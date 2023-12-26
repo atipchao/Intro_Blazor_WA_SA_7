0eaab3fd490d6a87e0db17b30b48f3cb0a2103af
atipchao <atip.chaogunhar@cookcountyil.gov>
2023-12-25 18:16:37 -0600

    Inplemented csv export from people index page

---
+++ Intro_Blazor_WA_SA_7/Client/Helpers/IJSRuntimeExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace Intro_Blazor_WA_SA_7.Client.Helpers
+{
+    public static class IJSRuntimeExtensions
+    {
+        public static ValueTask SaveAs(this IJSRuntime js, string fileName, byte[] content)
+        {
+            return js.InvokeVoidAsync("saveFileAs", fileName, Convert.ToBase64String(content));
+        }
+    }
+}
